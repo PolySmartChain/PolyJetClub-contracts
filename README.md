c406fc3a965cbc44eaf6c4b747448ec87a6f4792
yunmin <79829820+Hacker-Yun@users.noreply.github.com>
2022-03-22 10:28:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Pineapple Contracts
+
+## install
+
+```sh
+yarn install
+```
+
+## environment variable
+
+```sh
+export PRIVATE_KEY=***
+```
+
+## compile
+
+```sh
+yarn compile
+```
+
+## deploy
+
+```sh
+yarn deploy:<Custom Network>
+```
+
+## test
+
+```sh
+yarn test
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import "@nomiclabs/hardhat-waffle"
+import 'hardhat-deploy'
+import "solidity-coverage"
+
+export default {
+  solidity: {
+    version: '0.8.12',
+    settRings: {
+      optimizer: { enabled: true, runs: 200 },
+      evmVersion: 'istanbul',
+    },
+  },
+  networks: {
+    hardhat: {
+      gas: 120000000,
+      blockGasLimit: 120000000,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000
+    },
+    Rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
