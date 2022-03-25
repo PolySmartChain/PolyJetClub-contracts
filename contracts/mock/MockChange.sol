ae07063d1488b850ddd10544dde431e907891e84
yunmin <79829820+Hacker-Yun@users.noreply.github.com>
2022-03-25 10:59:59 +0800

    add Mock contracts

--- contracts/mock/MockChange.sol
+++ contracts/mock/MockChange.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
-import {IPineapple} from '../interface/IPineapple.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import {IPolyJetClub} from '../interface/IPolyJetClub.sol';
 import {IChange} from '../interface/IChange.sol';
 
 contract MockChange is IChange, Ownable {
@@ -14,7 +16,12 @@ contract MockChange is IChange, Ownable {
 
     uint256 public interval = 12 hours;
     uint256 public beginTime;
-    IPineapple public pineapple;
+    IPolyJetClub public pineapple;
+
+    IERC20 public erc20;
+    uint256 public per = 100 ether;
+
+    IERC721 public erc721;
 
     struct Attribute {
         uint256 attr;
@@ -31,7 +38,7 @@ contract MockChange is IChange, Ownable {
 
     constructor(address _pineapple, Attribute[] memory _attributes) Ownable() {
         beginTime = block.timestamp;
-        pineapple = IPineapple(_pineapple);
+        pineapple = IPolyJetClub(_pineapple);
         _setAttributes(_attributes);
     }
 
@@ -44,33 +51,58 @@ contract MockChange is IChange, Ownable {
         }
     }
 
-    function getLastTimestamp
-    (
-        TokenInfo memory tokenInfo,
-        uint256 tokenId
-    ) internal view returns (uint256 newLastTimestamp) {
+    function _getLastVIT(TokenInfo memory tokenInfo, uint256 tokenId) internal view returns (uint256) {
         uint256 date = pineapple.getDate(tokenId);
+        uint256 lastTimestamp;
         if (tokenInfo.lastTimestamp == 0) {//init
-            newLastTimestamp = beginTime > date ? beginTime : date;
+            lastTimestamp = beginTime > date ? beginTime : date;
         } else {
-            newLastTimestamp = tokenInfo.lastTimestamp;
+            lastTimestamp = tokenInfo.lastTimestamp;
         }
+        uint256 output = (lastTimestamp - block.timestamp) / interval;
+        return output > tokenInfo.vit ? 0 : tokenInfo.vit - output;
     }
 
     function getAttributes
     (
         TokenInfo memory tokenInfo,
-        uint256 lastTimestamp,
         uint256 tokenId
     ) internal view returns (uint256, string memory) {
-        uint256 output = (lastTimestamp - block.timestamp) / interval;
-        uint256 lastVIT = output > tokenInfo.vit ? 0 : tokenInfo.vit - output;
+        uint256 lastVIT = _getLastVIT(tokenInfo, tokenId);
         for (uint256 x = 0; x < attributes.length; x++) {
             if (attributes[x].attr <= lastVIT) {
                 return (lastVIT, string(abi.encodePacked(attributes[x].uri, tokenId.toString())));
             }
         }
         return (0, "");
+    }
+
+    //erc20
+    function addVIT(uint256 tokenId, uint256 amount) external {
+        require(amount >= per, "Error amount");
+        TokenInfo storage tokenInfo = tokenInfos[tokenId];
+        uint256 vit = _getLastVIT(tokenInfo, tokenId);
+        require(vit > 0, "Error VIT");
+        uint256 out = amount % per;
+        if (out != 0) {
+            amount = amount - out;
+        }
+        erc20.transferFrom(msg.sender, owner(), amount);
+
+        uint256 mult = amount / per;
+        tokenInfo.vit += mult;
+        tokenInfo.lastTimestamp = block.timestamp;
+    }
+
+    //erc721
+    function resurgence(uint256 tokenId, uint256 tokenId_721) external {
+        TokenInfo storage tokenInfo = tokenInfos[tokenId];
+        uint256 vit = _getLastVIT(tokenInfo, tokenId);
+        require(vit == 0, "Error VIT");
+
+        erc721.transferFrom(msg.sender, owner(), tokenId_721);
+        tokenInfo.vit = totalVIT;
+        tokenInfo.lastTimestamp = block.timestamp;
     }
 
     //from max to min
@@ -82,14 +114,22 @@ contract MockChange is IChange, Ownable {
         interval = _interval;
     }
 
+    function setERC20(address _token, uint256 _per) external onlyOwner {
+        erc20 = IERC20(_token);
+        per = _per;
+    }
+
+    function setERC721(address _erc721) external onlyOwner {
+        erc721 = IERC721(_erc721);
+    }
+
     function tokenURI
     (
         uint256 tokenId
     )
     external view override returns (string memory uri) {
         TokenInfo memory tokenInfo = tokenInfos[tokenId];
-        uint256 lastTimestamp = getLastTimestamp(tokenInfo, tokenId);
-        (, uri) = getAttributes(tokenInfo, lastTimestamp, tokenId);
+        (, uri) = getAttributes(tokenInfo, tokenId);
     }
 
     function getVIT
@@ -97,7 +137,6 @@ contract MockChange is IChange, Ownable {
         uint256 tokenId
     ) external view override returns (uint256 vit) {
         TokenInfo memory tokenInfo = tokenInfos[tokenId];
-        uint256 lastTimestamp = getLastTimestamp(tokenInfo, tokenId);
-        (vit,) = getAttributes(tokenInfo, lastTimestamp, tokenId);
+        vit = _getLastVIT(tokenInfo, tokenId);
     }
 }
---
+++ contracts/mock/MockERC721.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ERC721} from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract MockERC721 is ERC721 {
+
+    uint256 public tokenId;
+
+    constructor() ERC721("Mock", "Mock") {}
+
+    function claim() external {
+        _safeMint(msg.sender, ++tokenId);
+    }
+}
