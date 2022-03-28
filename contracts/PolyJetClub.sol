e80f9ce674e104cd675613d4606396286c133fda
yunmin <79829820+Hacker-Yun@users.noreply.github.com>
2022-03-28 14:22:09 +0800

    update fee for PolyJetClub.sol

--- contracts/PolyJetClub.sol
+++ contracts/PolyJetClub.sol
@@ -9,6 +9,7 @@ import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {ECDSA} from '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
 import {Address} from '@openzeppelin/contracts/utils/Address.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IChange} from './interface/IChange.sol';
 import {IPolyJetClub} from './interface/IPolyJetClub.sol';
 
@@ -18,11 +19,14 @@ contract PolyJetClub is ERC721Enumerable, EIP712, Ownable, IPolyJetClub {
 
     // keccak256("Permit(address from,uint256 tokenId)");
     bytes32 public constant PERMIT_TYPEHASH = 0xc242e34b93f9ad1ffc2c2c079dea5dccebcd284285197f32e072ea272cc3eef1;
-    //    uint256 public constant Fee = 50000 ether;
-    uint256 public constant Fee = 0.001 ether;// test
-    //    uint256 public constant WhiteFee = 1 ether;
-    uint256 public constant WhiteFee = 0.001 ether;//test
-    string public constant baseURI = "ipfs://";
+    uint256 public constant Fee = 0.001 ether;
+    uint256 public constant WDCFee = 12e11;
+    uint256 public constant WhiteFee = 1 ether;
+    uint256 public constant WDCWhiteFee = 100000000;
+    address public constant Black = 0x8888888888888888888888888888888888888888;
+    string public constant baseURI = "ipfs://QmSpBhCjD3EY82nxRaruMUBkGoSEihUA79WwdtDFipeSJJ";
+
+    IERC20 public constant WDC = IERC20(0x101D4507E0c07Aa929EF4Fd1eabcB7bcAef5e391);
 
     uint256 public total;
     address public change;
@@ -34,51 +38,76 @@ contract PolyJetClub is ERC721Enumerable, EIP712, Ownable, IPolyJetClub {
 
     constructor() ERC721("PolyJetClub", "PolyJetClub") EIP712("PolyJetClub", "1") Ownable() {}
 
-    function charges() internal returns (uint256) {
+    function charges(uint256 _fee) internal returns (uint256) {
         total++;
-        if (total <= 2000) return Fee;
-        return Fee * 2 ** (total / 2e03);
+        if (total <= 2e03) return _fee;
+        return _fee * 2 ** (total / 2e03);
     }
 
-    function claim(uint256 tokenId) external payable {
+    function claim(uint256 tokenId, uint256 amount) external payable {
         require(tokenId > 0 && tokenId < 9001, "Token ID invalid");
-        require(msg.value >= charges(), "Insufficient handling fee");
-
+        uint256 fee;
+        if (amount == 0) {
+            fee = msg.value;
+            require(fee >= charges(Fee), "Insufficient handling fee");
+            Address.sendValue(payable(Black), fee);
+        } else {//wdc
+            fee = amount;
+            require(fee >= charges(WDCFee), "WDC insufficient handling fee");
+            WDC.transferFrom(_msgSender(), Black, fee);
+        }
         date[tokenId] = block.timestamp;
         _safeMint(_msgSender(), tokenId);
-        Address.sendValue(payable(owner()), msg.value);
 
-        emit Claim(tokenId, _msgSender(), msg.value);
+        emit Claim(tokenId, _msgSender(), fee);
     }
 
-    function cliamBatch(uint256[] calldata tokenIds) external payable {
-        require(tokenIds.length > 0, "TokenIds invalid");
-        uint256 value1 = msg.value;
+    function cliamBatch(uint256[] calldata tokenIds, uint256 amount) external payable {
+        uint256 value1;
         uint256 value2;
+        uint256 fee;
+        if (amount == 0) {
+            value1 = msg.value;
+            fee = Fee;
+
+            Address.sendValue(payable(Black), value1);
+        } else {//wdc
+            value1 = amount;
+            fee = WDCFee;
+
+            WDC.transferFrom(_msgSender(), Black, value1);
+        }
         for (uint256 x = 0; x < tokenIds.length; x++) {
             uint256 tokenId = tokenIds[x];
             require(tokenId > 0 && tokenId < 9001, "Token ID invalid");
 
-            value2 += charges();
+            value2 += charges(fee);
             date[tokenId] = block.timestamp;
             _safeMint(_msgSender(), tokenId);
         }
         require(value1 >= value2, "Insufficient handling fee");
-        Address.sendValue(payable(owner()), value1);
 
-        emit ClaimBatch(tokenIds, _msgSender(), msg.value);
+        emit ClaimBatch(tokenIds, _msgSender(), value1);
     }
 
     function claimPermit(uint256 tokenId, bytes calldata _signature) external payable {
         require(tokenId > 9000 && tokenId < 10001, "Token ID invalid");
-        require(msg.value >= WhiteFee, "Insufficient handling fee");
+        uint256 fee;
+        if (msg.value != 0) {
+            fee = msg.value;
+            require(fee >= WhiteFee, "Insufficient handling fee");
+
+            Address.sendValue(payable(Black), fee);
+        } else {
+            fee = WDCWhiteFee;
+            WDC.transferFrom(_msgSender(), Black, fee);
+        }
         if (_msgSender() != owner()) {
             bytes32 digst = _hashTypedDataV4(keccak256(abi.encode(PERMIT_TYPEHASH, _msgSender(), tokenId)));
             require(digst.recover(_signature) == owner(), "Permit Failure");
         }
         date[tokenId] = block.timestamp;
         _safeMint(_msgSender(), tokenId);
-        Address.sendValue(payable(owner()), msg.value);
 
         emit Claim(tokenId, _msgSender(), msg.value);
     }
